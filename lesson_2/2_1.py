e76dc6d03da3448860defa46c8fe67542e90c020
Ivan Paranych <paran87@mail.ru>
2022-01-22 12:48:03 +0300

    String

--- lesson_2/2_1.py
+++ lesson_2/2_1.py
@@ -1,7 +1,6 @@
 str = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 for i in range(len(str)):
     if (str[i].isdigit()):
-        print(str[i])
         str[i] = '"' + str[i] + '"'
     elif (str[i][0] == '+'):
         str[i] = '"' + str[i] + '"'
