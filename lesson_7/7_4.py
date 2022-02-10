c963d7448453699c058801074250de510e1a799a
Ivan Paranych <paran87@mail.ru>
2022-02-10 16:51:51 +0300

    add 0 and +

--- lesson_7/7_4.py
+++ lesson_7/7_4.py
@@ -1,15 +1,18 @@
 import os
-from collections import defaultdict
 import django
 
-diction = defaultdict(int)
+diction = {}
+
 root_dir = django.__path__[0]
 print(root_dir)
 for root, dirs, files in os.walk(root_dir):
     for file in files:
         real_path = os.path.join(root,file)
         size =10 ** len(str(os.stat(real_path).st_size))
-        diction[size] += 1
+        if  size in diction.keys():
+            diction[size] += 1
+        else:
+            diction[size] = 1
 
 for size, nums in sorted(diction.items()):
     print(size, '   ', nums)
